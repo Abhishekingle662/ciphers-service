e813f23582e6816bc8bf9ecc5c56efeebe6ae88b
ABHISHEK NANDKUMAR INGLE <abhishekingle662@gmail.com>
2024-02-29 23:34:22 -0500

    clear

--- ciphers_project/ciphers_project/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import greeting, encode
-
-urlpatterns = [
-    path("", greeting, name="greeting"),
-    path("caesar/<str:plaintext>/<int:shift>", encode, name="encode")
-]
